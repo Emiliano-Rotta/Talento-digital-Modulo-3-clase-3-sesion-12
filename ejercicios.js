91759120fbf98c428934a8b549501bc6de068486
Emiliano Rotta <emilianorottaf@gmail.com>
2024-08-28 22:12:02 -0300

    consignas ejercicios

---
+++ ejercicios.js
@@ -0,0 +1,5 @@
+//Descripción: Crea una función que calcule el promedio de una lista de números. La función debe aceptar un número variable de argumentos utilizando parámetros rest.
+
+
+
+// Descripción: Crea una función que reciba un objeto con información del perfil de un usuario. La función debe utilizar desestructuración para extraer el nombre y la edad, y los parámetros rest para incluir una lista opcional de habilidades. Usa valores por defecto para la edad y habilidades.
